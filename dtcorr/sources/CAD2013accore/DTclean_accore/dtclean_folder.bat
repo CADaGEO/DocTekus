1d72bc3d850200b9b0c9b342684a167288e9cc00
crispil <cristel.legrand@cadageo.com>
2014-03-18 11:38:45 +0100

    Erratum

--- dtcorr/sources/CAD2013accore/DTclean_accore/dtclean_folder.bat
+++
@@ -1,5 +0,0 @@
-REM @ECHO Off
-ECHO DTclean_accore : Debut de nettoyage
-FOR /r %%f IN (*.dwg) DO "C:\Program Files\Autodesk\AutoCAD Map 3D 2013\accoreconsole.exe" /i "%%f" /s "D:\cglocal\sync\cg01\SI\Dev\DTclean_accore\dtclean.scr"
-
-pause
